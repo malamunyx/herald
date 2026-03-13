4fa40004232789d57582f2d8c68f3cc6c6d3828c
malamunyx <mikaelindrawan127@gmail.com>
2026-03-13 23:01:06 +1100

    integrated users router into main

--- api/src/main.py
+++ api/src/main.py
@@ -1,3 +1,8 @@
 from fastapi import FastAPI
 
+from src.config import settings
+from src.users.router import users_router
+
 app = FastAPI()
+
+app.include_router(users_router, prefix=settings.api_prefix)
